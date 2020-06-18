bffdc9057a8f9e80c5f36d1bf1af4abd71de7b85
User <user@Users-MacBook-Pro.local>
2020-06-18 11:43:12 -0700

    updates footer styling for shipping form

--- src/pages/Shipping/ShippingForm.js
+++ src/pages/Shipping/ShippingForm.js
@@ -25,123 +25,125 @@ class ShippingForm extends Component {
 
   render() {
     return (
-      <div className='admin-container'>
-        <h1 className ='admin-h1'>Enter Shipping Information</h1>
-        {this.renderRedirect()}
-        <form id='add-product-form'
-              action = " "
-              method = " "
-              onSubmit={(event) => {this.props.onShippingFormSubmit(event); this.setRedirect();}}
-        >   
-            <div className='input-field-container form-row'>
-              <label>Email
+      <div className='content-wrap'>
+        <div className='admin-container'>
+          <h1 className ='admin-h1'>Enter Shipping Information</h1>
+          {this.renderRedirect()}
+          <form id='add-product-form'
+                action = " "
+                method = " "
+                onSubmit={(event) => {this.props.onShippingFormSubmit(event); this.setRedirect();}}
+          >   
+              <div className='input-field-container form-row'>
+                <label>Email
+                    <br></br>
+                    <input className='add-product-input-field'
+                          id='shipping_email'
+                          name='shipping_email'
+                          value={this.props.value}
+                          placeholder='Email Address'
+                          onChange={(event) => this.props.handleShippingFormChange(event)}>
+                    </input>
+                  </label>
+              </div>
+
+              <div className='input-field-container form-row'>
+                <label>First Name
+                    <br></br>
+                    <input className='add-product-input-field'
+                          id='shipping_name_first'
+                          name='shipping_name_first'
+                          value={this.props.value}
+                          placeholder='First Name'
+                          onChange={(event) => this.props.handleShippingFormChange(event)}>
+                    </input>
+                  </label>
+              </div>
+
+              <div className='input-field-container form-row'>
+                <label>Last Name
                   <br></br>
                   <input className='add-product-input-field'
-                        id='shipping_email'
-                        name='shipping_email'
-                        value={this.props.value}
-                        placeholder='Email Address'
-                        onChange={(event) => this.props.handleShippingFormChange(event)}>
-                  </input>
-                </label>
-            </div>
-
-            <div className='input-field-container form-row'>
-              <label>First Name
-                  <br></br>
-                  <input className='add-product-input-field'
-                        id='shipping_name_first'
-                        name='shipping_name_first'
-                        value={this.props.value}
-                        placeholder='First Name'
-                        onChange={(event) => this.props.handleShippingFormChange(event)}>
-                  </input>
-                </label>
-            </div>
-
-            <div className='input-field-container form-row'>
-              <label>Last Name
-                <br></br>
-                <input className='add-product-input-field'
-                    id='shipping_name_last'
-                    name='shipping_name_last'
-                    value={this.props.value}
-                    placeholder='Last Name'
-                    onChange={(event) => this.props.handleShippingFormChange(event)}>
-                </input>
-              </label>
-            </div>
-
-            <div className='input-field-container form-row'>
-                <label>Street Address
-                  <br></br>
-                  <input className='add-product-input-field'
-                      id='shipping_street_address'
-                      name='shipping_street_address'
+                      id='shipping_name_last'
+                      name='shipping_name_last'
                       value={this.props.value}
-                      placeholder='Street Address'
+                      placeholder='Last Name'
                       onChange={(event) => this.props.handleShippingFormChange(event)}>
                   </input>
                 </label>
-            </div>
+              </div>
 
-            <div className='input-field-container form-row'>
-                <label>Apartment/ Unit Number
+              <div className='input-field-container form-row'>
+                  <label>Street Address
+                    <br></br>
+                    <input className='add-product-input-field'
+                        id='shipping_street_address'
+                        name='shipping_street_address'
+                        value={this.props.value}
+                        placeholder='Street Address'
+                        onChange={(event) => this.props.handleShippingFormChange(event)}>
+                    </input>
+                  </label>
+              </div>
+
+              <div className='input-field-container form-row'>
+                  <label>Apartment/ Unit Number
+                    <br></br>
+                    <input className='add-product-input-field'
+                        id='shipping_apt_unit'
+                        name='shipping_apt_unit'
+                        value={this.props.value}
+                        placeholder='Apt / Unit #'
+                        onChange={(event) => this.props.handleShippingFormChange(event)}>
+                    </input>
+                  </label>
+              </div>
+
+              <div className='input-field-container form-row'>
+                <label>City
                   <br></br>
                   <input className='add-product-input-field'
-                      id='shipping_apt_unit'
-                      name='shipping_apt_unit'
+                      id='shipping_city'
+                      name='shipping_city'
                       value={this.props.value}
-                      placeholder='Apt / Unit #'
+                      placeholder='City'
                       onChange={(event) => this.props.handleShippingFormChange(event)}>
                   </input>
                 </label>
-            </div>
+              </div>
 
-            <div className='input-field-container form-row'>
-              <label>City
-                <br></br>
-                <input className='add-product-input-field'
-                    id='shipping_city'
-                    name='shipping_city'
-                    value={this.props.value}
-                    placeholder='City'
-                    onChange={(event) => this.props.handleShippingFormChange(event)}>
-                </input>
-              </label>
-            </div>
+              <div className='input-field-container form-row'>
+                <label>State
+                  <br></br>
+                  <input className='add-product-input-field'
+                      id='shipping_state'
+                      name='shipping_state'
+                      value={this.props.value}
+                      placeholder='State'
+                      onChange={(event) => this.props.handleShippingFormChange(event)}>
+                  </input>
+                </label>
+              </div>
 
-            <div className='input-field-container form-row'>
-              <label>State
-                <br></br>
-                <input className='add-product-input-field'
-                    id='shipping_state'
-                    name='shipping_state'
-                    value={this.props.value}
-                    placeholder='State'
-                    onChange={(event) => this.props.handleShippingFormChange(event)}>
-                </input>
-              </label>
-            </div>
-
-            <div className='input-field-container form-row'>
-              <label>Zipcode
-                <br></br>
-                <input className='add-product-input-field'
-                    id='shipping_zipcode'
-                    name='shipping_zipcode'
-                    value={this.props.value}
-                    placeholder='Zipcode'
-                    onChange={(event) => this.props.handleShippingFormChange(event)}>
-                </input>
-              </label>
-            </div>
-           
+              <div className='input-field-container form-row'>
+                <label>Zipcode
+                  <br></br>
+                  <input className='add-product-input-field'
+                      id='shipping_zipcode'
+                      name='shipping_zipcode'
+                      value={this.props.value}
+                      placeholder='Zipcode'
+                      onChange={(event) => this.props.handleShippingFormChange(event)}>
+                  </input>
+                </label>
+              </div>
             
-            <div className='form-row'>
-                <button type='submit' className='submit-btn'>Submit and Checkout</button>
-            </div>
-        </form>
+              
+              <div className='form-row'>
+                  <button type='submit' className='submit-btn'>Submit and Checkout</button>
+              </div>
+          </form>
+        </div>
       </div>
     )
     }
--- src/pages/Shipping/index.js
+++ src/pages/Shipping/index.js
@@ -4,32 +4,14 @@ import ShippingForm from './ShippingForm'
 
 class Shipping extends Component {
     render() {
-        // if (this.props.cart.length === 0) {
-        //     return (
-        //         <div className='temp-testing'>
-        //             <h1>Your cart is empty</h1>
-        //         </div>
-        //     )
-        // } else {
             return (
                 <React.Fragment>
-                <div className='shipping-container'>
-                    <ShippingForm
-                        handleShippingFormChange = {this.props.handleShippingFormChange}
-                        onShippingFormSubmit = {this.props.onShippingFormSubmit}
-                        // cart = {this.props.cart}
-                        // removeProductFromCart = {this.props.removeProductFromCart}
-                    />
-                </div>
-                {/* <div className = 'cart-total-container'>
-                    <CartTotal 
-                        cart = {this.props.cart}
-                        cartQuantity = {this.props.cartQuantity}
-                        cartSubtotal = {this.props.cartSubtotal}
-                        calculateCartSubtotal = {this.props.calculateCartSubtotal}
-                        onCheckoutClick = {this.props.onCheckoutClick}
-                    />
-                </div> */}
+                    <div className='shipping-container'>
+                        <ShippingForm
+                            handleShippingFormChange = {this.props.handleShippingFormChange}
+                            onShippingFormSubmit = {this.props.onShippingFormSubmit}
+                        />
+                    </div>
                 </React.Fragment>
             )
         }
